fbbd314285b6bdbec7e153a308361638d0c99b28
GTDominic <seitheldominic@gmail.com>
2024-01-12 20:45:12 +0100

    dropdowns

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
-import './App.css';
+import React from "react";
+import "./App.css";
+
+import "@ui5/webcomponents/dist/Panel";
 
 function App() {
   return (
-    <div>
-      
+    <div className="app">
+      <section className="app-content">
+        <div className="dropdown-wrapper">
+          <ui5-panel header-text="Maze creation">
+            <p>Test</p>
+          </ui5-panel>
+        </div>
+        <div className="dropdown-wrapper">
+          <ui5-panel header-text="Solve the Maze Manually" collapsed={true}>
+            <p>Test</p>
+          </ui5-panel>
+        </div>
+        <div className="dropdown-wrapper">
+          <ui5-panel
+            header-text="Algorithmically Solve the Maze"
+            collapsed={true}
+          >
+            <p>Test</p>
+          </ui5-panel>
+        </div>
+      </section>
     </div>
   );
 }
