dc519ba4d77f1450c4203fe468e42e754c52b4fa
GTDominic <seitheldominic@gmail.com>
2024-01-25 16:52:24 +0100

    right wall algorithm

--- src/algorithms/FollowRightWall.js
+++ src/algorithms/FollowRightWall.js
@@ -15,6 +15,30 @@ class FollowRightWall {
   step() {
     if (this.board[this.focusPoint.row][this.focusPoint.column] === 3) return;
     if (!this.lastPosition) return this.firstStep();
+    const xdif = this.focusPoint.column - this.lastPosition.column;
+    const ydif = this.focusPoint.row - this.lastPosition.row;
+    const frow = this.focusPoint.row;
+    const fcolumn = this.focusPoint.column;
+    let direction; // 0: up, 1: left, 2: down, 3: right
+    let nrow, ncolumn;
+    if (xdif === 0) {
+      if (ydif === 1) direction = 0; // Last Move: down
+      else direction = 2; // Last Move: up
+    } else if (xdif === 1) direction = 1; // Last Move: right
+    else direction = 3; // Last Move: left
+    do {
+      direction = (direction + 1) % 4;
+      nrow = direction === 0 ? frow - 1 : direction === 2 ? frow + 1 : frow;
+      ncolumn = direction === 1 ? fcolumn - 1 : direction === 3 ? fcolumn + 1 : fcolumn;
+    } while (this.board[nrow][ncolumn] === 0);
+    if (this.board[nrow][ncolumn] !== 3) {
+      if (this.board[nrow][ncolumn] === 4) this.board[frow][fcolumn] = 1;
+      else this.board[nrow][ncolumn] = 4;
+    }
+    this.lastPosition = { row: frow, column: fcolumn };
+    this.focusPoint = { row: nrow, column: ncolumn };
+    this.setBoard(this.board);
+    this.setFocusPoint(this.focusPoint);
   }
 
   firstStep() {
@@ -37,9 +61,9 @@ class FollowRightWall {
       nrow = frow;
       ncolumn = fcolumn - 1;
     }
-    this.lastPosition = {row: frow, column: fcolumn};
+    this.lastPosition = { row: frow, column: fcolumn };
     this.board[nrow][ncolumn] = 4;
-    this.focusPoint = {row: nrow, column: ncolumn};
+    this.focusPoint = { row: nrow, column: ncolumn };
     this.setBoard(this.board);
     this.setFocusPoint(this.focusPoint);
   }
